d7981cf48e14e59b6ab76e1f9ac5ff4bcc399a1e
Vaibhav-Kambli <vaibhavk2608@gmail.com>
2021-11-06 17:12:17 -0400

    CRUD and mongodb connection exercise solution

--- exercises/models/crud.js
+++ exercises/models/crud.js
@@ -1,28 +1,34 @@
-const User = require('./user')
+const User = require("./user");
 
 const getUserById = (id) => {
- 
-}
+	return User.findById(id).exec();
+};
 
 const getAllUsers = () => {
-  
-}
+	return User.find({}).exec();
+};
 
 const createUser = (userDetails) => {
-  
-}
+	const user = new User(userDetails);
+	return user.save();
+	// second approach
+	//return User.create(userDetails)
+};
 const removeUserById = (id) => {
-  
-}
+	const user = User.findById(id);
+	return user.remove();
+	// second approach
+	// return User.findByIdAndRemove(id).exec();
+};
 
 const updateUserById = (id, update) => {
-
-}
+	return User.findByIdAndUpdate(id, update, { new: true });
+};
 
 module.exports = {
-  getUserById,
-  getAllUsers,
-  createUser,
-  removeUserById,
-  updateUserById
-}
+	getUserById,
+	getAllUsers,
+	createUser,
+	removeUserById,
+	updateUserById,
+};
